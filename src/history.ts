b63e8d782f805934eb0e6795f1144cf5c63036a2
corneluu <mcornel1178@gmail.com>
2026-06-03 12:21:29 +0300

    Update Pacem in terris audios and notifications history

--- src/history.ts
+++ src/history.ts
@@ -12,38 +12,38 @@ export interface UpdateHistory {
 }
 
 export const updateHistory: UpdateHistory = {
-  lastUpdate: '29 mai 2026',
+  lastUpdate: '3 iunie 2026',
   pdfs: [
     { 
       name: 'Ne-aduni pe toți cu iubire', 
       url: 'pdfs/cantec-16/partitura.pdf', 
       date: '29 mai 2026', 
       change: 'Partitură actualizată (s-a adăugat o măsură la finalul strofei)' 
-    },
-    { 
-      name: 'Eu te iubesc, Doamne', 
-      url: 'pdfs/cantec-13/partitura.pdf', 
-      date: '9 mai 2026', 
-      change: 'Partitură actualizată' 
     }
   ],
   audio: [
     { 
+      name: 'Pacem in terris (Sopran)', 
+      url: 'audio/cantec-11/sopran.mp3', 
+      date: '3 iunie 2026', 
+      change: 'Audio actualizat' 
+    },
+    { 
+      name: 'Pacem in terris (Alto)', 
+      url: 'audio/cantec-11/alto.mp3', 
+      date: '3 iunie 2026', 
+      change: 'Audio actualizat' 
+    },
+    { 
+      name: 'Pacem in terris (Tenor)', 
+      url: 'audio/cantec-11/tenor.mp3', 
+      date: '3 iunie 2026', 
+      change: 'Audio actualizat' 
+    },
+    { 
       name: 'Pacem in terris (Bas)', 
       url: 'audio/cantec-11/bas.mp3', 
-      date: '29 mai 2026', 
-      change: 'Audio actualizat' 
-    },
-    { 
-      name: 'La pace verrà (Alto)', 
-      url: 'audio/cantec-9/alto.mp3', 
-      date: '9 mai 2026', 
-      change: 'Audio actualizat' 
-    },
-    { 
-      name: 'La pace verrà (Sopran)', 
-      url: 'audio/cantec-9/sopran.mp3', 
-      date: '9 mai 2026', 
+      date: '3 iunie 2026', 
       change: 'Audio actualizat' 
     }
   ]
